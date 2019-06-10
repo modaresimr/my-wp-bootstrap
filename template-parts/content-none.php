c9043eb4ee531a27a5f7846b71759a51f0136cf1
alim1369 <alim1369@gmail.com>
2019-06-10 20:25:29 +0200

    set ui to fluid

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -20,9 +20,8 @@
 			if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 				<p><?php
-					printf(
+					/*printf(
 						wp_kses(
-							/* translators: 1: link to WP admin new post page. */
 							__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wp-bootstrap-4' ),
 							array(
 								'a' => array(
@@ -31,20 +30,20 @@
 							)
 						),
 						esc_url( admin_url( 'post-new.php' ) )
-					);
+					);*/
 				?></p>
 
 			<?php elseif ( is_search() ) : ?>
 
 				<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wp-bootstrap-4' ); ?></p>
 				<?php
-					get_search_form();
+					//get_search_form();
 
 			else : ?>
 
 				<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'wp-bootstrap-4' ); ?></p>
 				<?php
-					get_search_form();
+					//get_search_form();
 
 			endif; ?>
 		</div><!-- .page-content -->
